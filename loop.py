4226985f28e449268cf72ebd430227c4e3460661
rohit044 <2022pcecrrohit045@poornima.org>
2025-01-29 19:53:04 -0800

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,115 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "c7cb813c-5563-4381-af30-55fe98b57feb",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "red\n",
+      "green\n",
+      "white\n",
+      "purple\n",
+      "pink\n",
+      "marron\n",
+      "g\n",
+      "o\n",
+      "e\n",
+      "d\n",
+      "h\n",
+      "u\n",
+      "b\n",
+      " \n",
+      "t\n",
+      "e\n",
+      "c\n",
+      "h\n",
+      "n\n",
+      "o\n",
+      "l\n",
+      "o\n",
+      "g\n",
+      "i\n",
+      "e\n",
+      "s\n",
+      "1\n",
+      "q\n",
+      "2\n",
+      "w\n",
+      "3\n",
+      "e\n",
+      "4\n",
+      "r\n",
+      "1\n",
+      "3\n",
+      "6\n",
+      "10\n",
+      "15\n",
+      "21\n",
+      "28\n",
+      "36\n",
+      "45\n",
+      "55\n"
+     ]
+    }
+   ],
+   "source": [
+    "# for loop in list\n",
+    "color_list = ['red','green','white','purple','pink','marron']\n",
+    "for i in color_list:\n",
+    "    print(i)\n",
+    "# for loop in string \n",
+    "fr_string = \"goedhub technologies\"\n",
+    "for i in fr_string:\n",
+    "    print(i)\n",
+    "\n",
+    "# for loop in dictionary\n",
+    "fr_dict = {1:'q',2:'w',3:'e',4:\"r\"}\n",
+    "for i in fr_dict:\n",
+    "    print(i)\n",
+    "    print(fr_dict[i])\n",
+    "\n",
+    "# sum of all list elements\n",
+    "Num = [1,2,3,4,5,6,7,8,9,10]\n",
+    "sum = 0\n",
+    "for i in Num:\n",
+    "    sum = sum + i\n",
+    "    print(sum)\n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "99a80d49-e820-4813-81fe-d0c3b6a8630c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
